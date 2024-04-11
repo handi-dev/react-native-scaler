c3ba3d8f2a343ba1a2fd3a0263e2a7ab6a781feb
Handi Priyono <handimacbook@gmail.com>
2024-04-11 18:24:50 +0700

    feat: add example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,31 +1,91 @@
-import * as React from 'react';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { initScalers } from 'react-native-scaler';
+const { moderateScale, ScaledSheet } = initScalers();
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-scaler';
-
-export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
-
-  return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
+const scaledStyles = ScaledSheet.create({
   container: {
     flex: 1,
+    width: '100%',
+    height: '100%',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
+  button: {
+    width: moderateScale(250),
+    height: '80@ms',
+    borderColor: '#696969',
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: '25@vs',
+    elevation: 3,
+    shadowColor: 'black',
+    shadowOpacity: 0.25,
+    shadowRadius: 2,
+    shadowOffset: {
+      height: 1,
+      width: -1,
+    },
+  },
+  text: {
+    fontSize: '20@ms0.3',
+    fontWeight: 'bold',
   },
 });
+
+const regularStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    width: 250,
+    height: 80,
+    borderColor: '#696969',
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 25,
+    elevation: 3,
+    shadowColor: 'black',
+    shadowOpacity: 0.25,
+    shadowRadius: 2,
+    shadowOffset: {
+      height: 1,
+      width: -1,
+    },
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
+
+const Home = () => {
+  const [isRegular, setIsRegular] = useState(true);
+  let styles = isRegular ? regularStyles : scaledStyles;
+  const onChange = () => {
+    setIsRegular((prev) => !prev);
+  };
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.button} onPress={() => {}}>
+        <Text style={styles.text}>First Example</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={() => {}}>
+        <Text style={styles.text}>Second Example</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={onChange}>
+        <Text>Switch to: </Text>
+        <Text style={styles.text}>{isRegular ? 'Scaled' : 'Regular'}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Home;
